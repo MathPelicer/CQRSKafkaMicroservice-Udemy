8994643e2eb033ed23401b52c62c4c725efcc21f
Matheus L. G. Pelicer <matheuspelicer58@gmail.com>
2023-09-27 19:52:22 -0300

    feat: section 9 - domain layer

---
+++ src/SM.Post/Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task CreateAsync(CommentEntity commentEntity);
+
+        Task UpdateAsync(CommentEntity commentEntity);
+
+        Task<CommentEntity> GetByIdAsync(Guid id);
+
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ src/SM.Post/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,24 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity postEntity);
+
+        Task UpdateAsync(PostEntity postEntity);
+
+        Task DeleteAsync(Guid id);
+
+        Task<PostEntity> GetByIdAsync(Guid id);
+
+        Task<List<PostEntity>> GetAllAsync();
+
+        Task<List<PostEntity>> GetAllByAuthorAsync(string author);
+
+        Task<List<PostEntity>> GetAllByLikesAsync(int likes);
+
+        Task<List<PostEntity>> GetAllByCommentsAsync(int likes);
+
+    }
+}
